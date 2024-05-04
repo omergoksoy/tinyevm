a01dbacee3a58c956e79b30617b48a9cda572a29
Omer GOKSOY <goksoy.omer@gmail.com>
2024-05-04 15:28:20 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use tinyevm::{
 
 fn main() {
     let opcodes = lex_bytecode("0x6001600101").unwrap();
+    // let opcodes = lex_bytecode("0x60 01 60 01 01").unwrap();
+    println!("{:?}", opcodes.clone());
     let (mut stack, _storage, _memory) = eval_opcode(opcodes);
     let (last, rest) = stack.pop();
     assert_eq!(last.unwrap(), to_uint256(&[0x02], Endian::Little));
